b41f27fbef5b0af27ddcd3d8e866ae00d3071505
鄭又誠 <zxcy9980@gmail.com>
2020-07-07 16:19:30 +0800

    自訂 Handlebars Helper：ifCond

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-app.engine('handlebars', handlebars({defaultLayout:'main'}))
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+  helpers: require('./config/handlebars-helpers')
+}))
 app.set('view engine', 'handlebars')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
@@ -24,14 +27,14 @@ app.use(methodOverride('_method'))
 app.use('/upload', express.static(__dirname + '/upload'))
 
 app.use((req, res, next) => {
-    res.locals.success_messages = req.flash('success_messages')
-    res.locals.error_messages = req.flash('error_messages')
-    res.locals.user = req.user
-    next()
-  })
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  next()
+})
 
 
-app.listen(port, () => {    
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
 
