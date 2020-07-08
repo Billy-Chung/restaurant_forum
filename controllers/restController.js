a4a233316d2b1de85bc78dc0b0d5058187871c6f
鄭又誠 <zxcy9980@gmail.com>
2020-07-08 16:58:21 +0800

    完成前台畫面

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,7 +1,19 @@
-const restController = {
-    getRestaurants: (req, res) => {
-        return res.render('restaurants')
-    }
-}
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
 
+let restController = {
+  getRestaurants: (req, res) => {
+    Restaurant.findAll({ include: Category }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r.dataValues,
+        description: r.dataValues.description.substring(0, 50),
+        categoryName: r.Category.name
+      }))
+      return res.render('restaurants', {
+        restaurants: data
+      })
+    })
+  }
+}
 module.exports = restController
