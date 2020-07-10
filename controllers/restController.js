a50fca76aefbaf91ebea747bf817e417303a989c
鄭又誠 <zxcy9980@gmail.com>
2020-07-10 14:27:44 +0800

    餐廳列表分頁功能

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,33 +1,45 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
 
 let restController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category }).then(restaurants => {
-      let whereQuery = {}
-      let categoryId = ''
-      if (req.query.categoryId) {
-        categoryId = Number(req.query.categoryId)
-        whereQuery['CategoryId'] = categoryId
-      }
-
-      Restaurant.findAll(({ include: Category, where: whereQuery })).then(restaurants => {
-        // ..skip
-        const data = restaurants.map(r => ({
-          ...r.dataValues,
-          description: r.dataValues.description.substring(0, 50),
-          categoryName: r.Category.name
-        }))
-        Category.findAll({
-          raw: true,
-          nest: true
-        }).then(categories => { // 取出 categoies 
-          return res.render('restaurants', {
-            restaurants: data,
-            categories: categories,
-            categoryId: categoryId
-          })
+    let offset = 0
+    let whereQuery = {}
+    let categoryId = ''
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery['categoryId'] = categoryId
+    }
+    Restaurant.findAndCountAll({ include: Category, where: whereQuery, offset: offset, limit: pageLimit }).then(result => {
+      // data for pagination
+      let page = Number(req.query.page) || 1
+      let pages = Math.ceil(result.count / pageLimit)
+      let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      let prev = page - 1 < 1 ? 1 : page - 1
+      let next = page + 1 > pages ? pages : page + 1
+      // clean up restaurant data
+      const data = result.rows.map(r => ({
+        ...r.dataValues,
+        description: r.dataValues.description.substring(0, 50),
+        categoryName: r.Category.name
+      }))
+      Category.findAll({ 
+        raw: true,
+        nest: true
+      }).then(categories => {
+        return res.render('restaurants', {
+          restaurants: data,
+          categories: categories,
+          categoryId: categoryId,
+          page: page,
+          totalPage: totalPage,
+          prev: prev,
+          next: next
         })
       })
     })
