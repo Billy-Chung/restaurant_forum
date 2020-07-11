36c24709aedeeac24077ab2f79404e62fa12ef05
鄭又誠 <zxcy9980@gmail.com>
2020-07-11 18:22:39 +0800

    顯示瀏覽數

---
+++ migrations/20200711101755-add-viewsCounts-to-restaurants.js
@@ -0,0 +1,12 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn( 'Restaurants', 'viewCounts', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn( 'Restaurants', 'viewCounts' );
+  }
+};
